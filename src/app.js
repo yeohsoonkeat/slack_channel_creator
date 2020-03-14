b7dcdc3110ac22df67fdce32b8c31e2d16869a4d
Yeoh Soon Keat <yeohsoonkeat18@kit.edu.kh>
2020-03-14 17:55:41 +0700

    init

---
+++ src/app.js
@@ -0,0 +1,16 @@
+require("dotenv").config()
+const { App } = require("@slack/bolt")
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    endpoints: {
+      events: "/slack/events",
+      commands: "/slack/commands"
+    }
+  });
+
+(async () => {
+    await app.start(process.env.PORT || 3000);
+    console.log("⚡️ Bolt app is running!");
+})();
